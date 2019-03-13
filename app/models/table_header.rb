cf38f93568ac8253c14108d9aa287977781b6259
Nikolay G Titov <nikolaygtitov@gmail.com>
2019-03-13 19:46:35 -0400

    E1920 Fix Code Climate issues: table_header.rb   - 2 out of 3 issues are resolved in the table_header.rb file   - 1 remaining issue is reported as False Positive in Rails 4 and can     be ignored     * Mass assignment is not restricted using attr_accessible     * Confirmed with Zachariah Mathews

--- app/models/table_header.rb
+++ app/models/table_header.rb
@@ -1,13 +1,24 @@
+# E1920
+# Code Climate mistakenly reports
+# "Mass assignment is not restricted using attr_accessible"
+# https://github.com/presidentbeef/brakeman/issues/579
+#
+# Remove html_safe and eliminate DRY code
+
 class TableHeader < QuestionnaireHeader
+  # Code Climate suggestion to use safe_join instead of html_safe
   def complete(_count, _answer = nil)
-    html = '<br/><big><b>' + self.txt + '</b></big><br/>'
-    html += '<table class="general" style="border: 2; text-align: left; width: 100%">'
-    html.html_safe
+    safe_join(make_header)
   end
 
   def view_completed_question(_count, _answer)
-    html = '<br/><big><b>' + self.txt + '</b></big><br/>'
-    html += '<table class="general" style="border: 2; text-align: left; width: 100%">'
-    html.html_safe
+    safe_join(make_header)
+  end
+
+  private
+
+  def make_header
+    ['<br/><big><b>', self.txt, '</b></big><br/>',
+     '<table class="general" style="border: 2; text-align: left; width: 100%">']
   end
 end
